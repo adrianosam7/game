5ec82dae3e33f4bd7d21817f9a0e2d3e957322d0
adrianosam7 <adrianosam7@live.com>
2023-05-12 03:19:10 -0400

    criado as aplication, aplication-test e classe Game.java

---
+++ src/main/java/com/sam7/gamelist/entities/Game.java
@@ -0,0 +1,143 @@
+package com.sam7.gamelist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/*configura a classe java para que ela seja equivalente a uma tabela do banco relacional*/
+@Entity 
+
+/*contumiza o nome da tabela*/
+@Table(name = "tb_game")
+public class Game {
+	
+	//configurando o id como chave primaria//
+	@Id 
+	
+	//para o id ser auto-incrementavel pelo banco dados//
+	@GeneratedValue (strategy = GenerationType.IDENTITY) 
+	
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")//constumizando o nome da coluna ano pois year é uma palavra reservada do banco de dados (SQL)// 
+	private Integer year;
+	private String genre;
+	private String platform;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+
+	
+	/*construtor sem argumento*/
+	public Game() {
+	
+	}
+	
+	/*construtor com argumentos para estaciar um game já passando os argumentos  - (constructor using fields)(opcional)*/ 
+	
+	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+			String shortDescription, String longDescription) {
+		
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+		this.platform = platform;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
+
+	/*Getters e Setters*/
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	//metodo equals e hash code usado para comparar dois games se eles são iguais ou não no caso//
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
