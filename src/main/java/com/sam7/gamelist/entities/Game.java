e04e706fe0aa8c3441e6969b5bd76e6f4e95780a
adrianosam7 <adrianosam7@live.com>
2023-05-16 19:36:39 -0400

    teste de inserção dos dados na tabela - game seed

--- src/main/java/com/sam7/gamelist/entities/Game.java
+++ src/main/java/com/sam7/gamelist/entities/Game.java
@@ -22,9 +22,12 @@ public class Game {
 	@Column(name = "game_year")//constumizando o nome da coluna ano pois year é uma palavra reservada do banco de dados (SQL)// 
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")//INSTRUÇÃO PARA A JPA GERE O CAMPO COMO TEXTO PARA NÃO LIMITAR O CAMPO PARA APENAS 255 CARACTERES//
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")//INSTRUÇÃO PARA A JPA GERE O CAMPO COMO TEXTO PARA NÃO LIMITAR O CAMPO PARA APENAS 255 CARACTERES//
 	private String longDescription;
 
 	
@@ -35,14 +38,15 @@ public class Game {
 	
 	/*construtor com argumentos para estanciar um game já passando os argumentos  - (constructor using fields)(opcional)*/ 
 	
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -81,14 +85,22 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platform) {
+		this.platforms = platform;
+	}
+	
+	public Double getScore() {
+		return score;
 	}
 
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	
 	public String getImgUrl() {
 		return imgUrl;
 	}
